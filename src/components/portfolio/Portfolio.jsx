95ef17b001d5b2612c67e606f8b132b51fef4dee
Khaled <101639047+khaled899@users.noreply.github.com>
2024-10-22 11:18:17 +0300

    edit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,28 +8,28 @@ import IMG6 from "../../assets/Education.png";
 import IMG7 from "../../assets/Movies.png";
 import IMG8 from "../../assets/Paralex.png";
 import IMG9 from "../../assets/course.png";
+import IMG10 from "../../assets/Ella.png";
+import IMG11 from "../../assets/amit.png";
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: "Wooster Template",
+    title: "Wooster Template [HTML,CSS]",
     github: "https://github.com/khaled899/Wooster",
     URL: "https://khaled899.github.io/Wooster/",
-    status: "Live",
+    status: "Live Demo",
   },
   {
     id: 2,
     image: IMG2,
-    title: "University React App [HTML , SCSS , React.js]",
+    title: "University [HTML , SCSS , React.js]",
     github: "https://github.com/khaled899/University-.git",
-    URL: "/",
-    status: "Live Demo",
   },
   {
     id: 3,
     image: IMG3,
-    title: "Kasper CSS and HTML Template",
+    title: "Kasper [HTML,CSS]",
     github: "https://github.com/khaled899/Kasper",
     URL: "https://khaled899.github.io/Kasper/",
     status: "Live Demo",
@@ -37,7 +37,7 @@ const data = [
   {
     id: 4,
     image: IMG4,
-    title: "Games Template",
+    title: "Games Template [HTML,CSS]",
     github: "https://github.com/khaled899/Games",
     URL: "https://khaled899.github.io/Games/",
     status: "Live Demo",
@@ -45,7 +45,7 @@ const data = [
   {
     id: 5,
     image: IMG6,
-    title: "Education Template",
+    title: "Education Template [HTML,CSS]",
     github: "https://github.com/khaled899/Equcation",
     URL: "https://khaled899.github.io/Equcation/",
     status: "Live Demo",
@@ -53,7 +53,7 @@ const data = [
   {
     id: 6,
     image: IMG7,
-    title: "Movies HTML , CSS , JavaScript , API",
+    title: "Movies [HTML , CSS , JavaScript , API]",
     github: "https://github.com/khaled899/Movies",
     URL: "https://khaled899.github.io/Movies/",
     status: "Live Demo",
@@ -61,7 +61,7 @@ const data = [
   {
     id: 7,
     image: IMG8,
-    title: "Paralex HTML , CSS , JavaScript",
+    title: "Paralex [HTML , CSS , JavaScript]",
     github: "https://github.com/khaled899/Paralex.git",
     URL: "https://khaled899.github.io/Paralex/",
     status: "Live Demo",
@@ -74,6 +74,22 @@ const data = [
     URL: "https://khaled899.github.io/Courses-Static-Version-/",
     status: "Live Demo",
   },
+  {
+    id: 9,
+    image: IMG10,
+    title: "Ella-Mart [Vue.js]",
+    github: "https://github.com/khaled899/Ella-Mart.git",
+    URL: "https://ella-mart.web.app/",
+    status: "Live Demo",
+  },
+  {
+    id: 8,
+    image: IMG11,
+    title: "AMIT-Learning",
+    github: "",
+    URL: "https://amit-learning.com/",
+    status: "Live Demo",
+  },
 ];
 export const Portfolio = () => {
   return (
@@ -82,20 +98,28 @@ export const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio-container">
-        {data.map(({ id, image, title, github, URL, status }) => {
+        {data.map((data) => {
           return (
-            <article key={id} className="portfolio-item">
+            <article key={data.id} className="portfolio-item">
               <div className="image">
-                <img src={image} alt={title} />
+                <img src={data.image} alt={data.title} />
               </div>
-              <h3>{title}</h3>
+              <h3>{data.title}</h3>
               <div className="pcta">
-                <a href={github} className="btn" target="_blank" rel="noreferrer">
-                  GitHub
-                </a>
-                <a href={URL} className="btn btn-primary" target="_blank" rel="noreferrer">
-                  {status}
-                </a>
+                {data.github ? (
+                  <a href={data.github} className="btn" target="_blank" rel="noreferrer">
+                    GitHub
+                  </a>
+                ) : (
+                  ""
+                )}
+                {data.URL ? (
+                  <a href={data.URL} className="btn btn-primary" target="_blank" rel="noreferrer">
+                    {data.status}
+                  </a>
+                ) : (
+                  ""
+                )}
               </div>
             </article>
           );
